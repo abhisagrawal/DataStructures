eaee1a3d7c4909a36511a0e38405f71bea487339
abhisagrawal <abhisagrawal@stubcorp.com>
2014-08-31 20:38:57 -0700

    Recurrsion and Backtracking

---
+++ RecurrsionAndBacktracking/src/String/StringReversal.java
@@ -0,0 +1,23 @@
+package String;
+
+public class StringReversal {
+
+	static StringBuilder sb = new StringBuilder("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+	
+	public static void reverseString(int pos){
+		if(pos==sb.length()){
+			sb = new StringBuilder();
+		}else{
+			char temp = sb.charAt(pos);
+			reverseString(pos+1);
+			sb.append(temp);
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		reverseString(0);
+		System.out.println(sb);
+	}
+
+}
